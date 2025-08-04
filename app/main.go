1d2452283dfbbbbbcebf168c6204b6f67ef9f574
Nghia Dang <nghiadangtran1@gmail.com>
2025-08-04 13:47:07 -0400

    test

--- app/main.go
+++ app/main.go
@@ -7,16 +7,12 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -31,7 +27,8 @@ func main() {
 			os.Exit(1)
 		}
 
-		HandelConnection(conn)
+		go HandelConnection(conn)
+
 	}
 }
 
